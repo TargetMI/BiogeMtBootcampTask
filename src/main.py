4c5c1c6aa5d69fcb9354514ad1d3bd63df2e95ec
Matthew Drago <matthew.drago.16@um.edu.mt>
2023-12-12 14:10:55 +0100

    Added the output directory

--- src/main.py
+++ src/main.py
@@ -6,12 +6,22 @@ from counting import count_seq
 @click.command()
 @click.option('--background_fasta', help='The fasta file containing the background sequences to be parsed and included')
 @click.option('--motif_fasta', help='The fasta file containing the motifs to be parsed and included')
-def main(background_fasta, motif_fasta):
+@click.option('--output_directory', help='The output directory holding the result')
+def main(background_fasta, motif_fasta, output_directory) -> None:
+    """
+    The main function which will action the pipeline
+    :param background_fasta: the background fasta file
+    :param motif_fasta: the motifs fasta file
+    :param output_directory: the output directory holding the results
+    :return: None
+    """
+    # add a check that the args are fasta
+    if not (background_fasta.endswith('.fa') or motif_fasta.endswith('.fa')):
+        raise ValueError('Not a valid format')
     background_seq = IO.to_dict(IO.parse(background_fasta, "fasta"))
     motif_seq = IO.to_dict(IO.parse(motif_fasta, "fasta"))
     background_counts = [count_seq(seq) for seq in background_seq]
     motif_counts = [count_seq(seq) for seq in motif_seq]
-    pass
 
 
 if __name__ == '__main__':
